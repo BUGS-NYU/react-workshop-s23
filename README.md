2964258888fcbabee1d1b47d7d1cf119b0b17d64
nedjulius <julius.nedzinskas@gmail.com>
2023-03-31 12:58:08 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# React Workshop Template
+
+## Instructions
+
+1. Clone this repository
+2. Run `npm install` in the root of this repository
+3. Run `npm run dev` to start the development server and hot-reloading
+4. Navigate to `src/App.jsx` and follow the instructions in the comments to complete this exercise
+
+## Objectives of the workshop
+
+- Familiarize yourself with the basics of React such as state and conditional rendering
+- Create a working application that you can utilize for personal use
+- Have fun and hopefully learn something new!⚡️
+
+## What to do if you are stuck?
+
+In case you are stuck and not sure what to do, feel free to reach out to Bugs members IRL or in the Discord chat!
+
+## Important files
+- `src/App.jsx` - main component in which all the website logic is defined
+- `src/App.css` - stylesheet that is used by the `App` component (you are not required to make any changes there, but feel free to do whatever changes you want!)
+
---
+++ src/App.jsx
@@ -0,0 +1,84 @@
+// Imports that are required for this app to work
+// Feel free to replace avatar with any other image
+// If you decide to use your own image, make sure to upload it to src/assets
+import { useState } from 'react';
+import avatarPic from './assets/avatar.png';
+import './App.css';
+
+// String constants that define the theme of the website
+const LIGHT = 'LIGHT';
+const DARK = 'DARK';
+
+const LINKS = [
+  { title: 'Website', url: 'https://www.bugsnyu.com' }
+  // Extend this array with your desired links
+  // { title: string, url: string }
+];
+
+// Requires two props: `imageSrc` and `name`
+// `imageSrc` - avatar image import from the assets
+// `name` - the desired name that will be displayed below the avatar
+const Avatar = ({ imageSrc, name }) => (
+  <>
+    <img src={imageSrc} alt='' className='avatar' />
+    <h1>{name}</h1>
+  </>
+);
+
+// Requires two props: `title` and `url`
+// `title` - link button title
+// `url` - the url that the link button links to
+const LinkButton = ({ title, url }) => (
+  <a target='_blank' href={url} className='link'>
+    {title}
+  </a>
+);
+
+// Require a single prop: `toggleTheme`
+// `toggleTheme` - reference to a function that will toggle the state of the theme
+const ThemeTogglerButton = ({ toggleTheme }) => (
+  <button className='theme-toggler' onClick={toggleTheme}>
+    Toggle theme
+  </button>
+);
+
+const App = () => {
+  // State hook that is responsible for holding the information about website's current theme
+  // `currentTheme` variable is where you access the data about the current theme
+  // `setCurrentTheme` is a function that allows you to change the state
+  // On initial render of the component, the theme will be set to `DARK`
+  const [currentTheme, setCurrentTheme] = useState(DARK);
+
+  // Constant var that maps all of the links
+  // Hint: use <LinkButton /> component and `.map` method for `LINKS` array
+  // Docs reference: https://react.dev/learn/conditional-rendering
+  const mapLinks = null; // TODO: remove `null` and complete this constant
+
+  // Constant var that holds the appropriate className string based on the theme state
+  // If the currentTheme is `DARK` then the className string should be `container`
+  // If the currentTheme is `LIGHT` then the className string should be `container light`
+  // Hint: use ternary operator and check the `currentTheme` value
+  // Docs reference: https://react.dev/learn/conditional-rendering
+  const containerClassName = null; // TODO: remove `null` and complete this constant
+
+  // Function that toggles the state of the current theme
+  // If the current theme is `DARK` then toggle to `LIGHT`, else toggle to `DARK`
+  // Hint: use `setCurrentTheme` to toggle the state
+  // Docs reference: https://react.dev/learn/state-a-components-memory
+  const toggleTheme = () => {}; // TODO: complete this function
+
+  return (
+    <div className={containerClassName}>
+      {
+        /*
+          TODO: Complete the App component here
+          - Add <ThemeTogglerButton /> and pass the required props
+          - Add <Avatar /> component and pass the required props
+        */
+      }
+      {mapLinks}
+    </div>
+  );
+};
+
+export default App;
